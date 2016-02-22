f2860f2172f15be9aef29a4f6c514a6baa5cccd3
Paul Bland <paul.bland@gmail.com>
2016-02-22 20:02:59 +1100

    stuff

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
 
 $(function() {
 
-	var currentZIndex = 0;
-
-	//$('card').draggable();
-
-	// $('card').on('mousedown', function() {
-	// 	$(this).css('zIndex', ++currentZIndex);
-	// 	$('nav').css('zIndex', (currentZIndex+1));
-	// 	$('.status').css('zIndex', (currentZIndex+2));
-	// });
-
 	$('nav').on('click', function() {
 		$('body').addClass('nav-open')
 	})
