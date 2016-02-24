4424f0f6ed1db67c5495a6ca917445dc68e57b1d
Paul Bland <paul.bland@gmail.com>
2016-02-25 09:37:55 +1100

    change color with click - and made classic/modern colors the same

--- public/js/app.js
+++ public/js/app.js
@@ -76,9 +76,11 @@ $(function() {
 
 		tagName: 'li',
 
-		className: function() {
-			return 'card style-' + this.model.get('color');
-		},
+		// className: function() {
+		// 	return 'card style-' + this.model.get('color');
+		// },
+
+		className: 'card',
 
 		template: _.template( $('#card-template').html() ),
 
@@ -88,19 +90,26 @@ $(function() {
 
 	 		this.$input = this.$el.find('textarea');
 
+	 		// needed this when class was a function
+	 		// style now moved to wrapper class
+	 		// this.$el.attr('class', _.result(this, 'className'));
+
+
 	 		return this;
 		},
 
 		initialize: function(){
 
 			 this.listenTo(this.model, 'destroy', this.remove);
+			 this.listenTo(this.model, 'change', this.render);
 			
   
 		},
 
 		events: {
 			'keyup textarea' : 'textareaKeyup',
-			'click a.delete-card' : 'deleteCard'
+			'click a.delete-card' : 'deleteCard',
+			'click a.set-card-style' : 'setCardStyle'
 		},
 
 		textareaKeyup : function(e) {
@@ -114,6 +123,15 @@ $(function() {
 		deleteCard: function(e) {
 			e.preventDefault();
 			this.model.destroy();
+		},
+
+
+		setCardStyle: function(e) {
+			e.preventDefault();
+			// could also do set here - then save elsewere?
+			// or throttle saves somehow?
+			this.model.save('color', $(e.target).attr('href'));
+
 		}
 
 	});
--- public/js/main.js
+++ public/js/main.js
@@ -2,16 +2,10 @@
 $(function() {
 
 	$('nav').on('click', function() {
-		$('body').addClass('nav-open');
+		$('body').toggleClass('nav-open');
 	});
 
-	$('main').on('click', function() {
-		$('body').removeClass('nav-open');
-	});
-
-
-	$('[name="theme"]').on('change', function(e) {
-
+	$('nav input[name="theme"]').on('change', function(e) {
 		$('body').toggleClass('modern', $(e.target).val() === "modern");
 		$('body').toggleClass('classic', $(e.target).val() === "classic");
 	});
