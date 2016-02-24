744681fecb4920ff39a7144be18e3a8fa5776a69
Paul Bland <paul.bland@gmail.com>
2016-02-24 13:51:36 +1100

    auth now in its own file

--- api/auth.js
+++ api/auth.js
@@ -5,22 +5,27 @@
 // from here http://www.codexpedia.com/node-js/node-js-basic-auth-in-express-js/
 // and also npom install basic-autth
 // woo
-var basicAuth = require('basic-auth');
 
-var auth = function (req, res, next) {
-  var user = basicAuth(req);
-  if (!user || !user.name || !user.pass) {
-    res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
-    res.sendStatus(401);
-    return;
-  }
-  if (user.name === 'paul' && user.pass === 'scribo825') {
-    next();
-  } else {
-    res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
-    res.sendStatus(401);
-    return;
-  }
-};
+module.exports = (function() {
 
-module.exports = auth;
+	var basicAuth = require('basic-auth');
+
+	var auth = function (req, res, next) {
+	  var user = basicAuth(req);
+	  if (!user || !user.name || !user.pass) {
+	    res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+	    res.sendStatus(401);
+	    return;
+	  }
+	  if (user.name === 'paul' && user.pass === 'scribo825') {
+	    next();
+	  } else {
+	    res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+	    res.sendStatus(401);
+	    return;
+	  }
+	};
+
+	return auth;
+
+})();
--- server.js
+++ server.js
@@ -15,25 +15,12 @@ mongoose.connect('scribo:69Sal6Iab4SWc9D@ds015398.mongolab.com:15398/scribo'); /
 
 
 
-// var auth     = require('./api/auth');
+ var auth     = require('./api/auth');
 
-var basicAuth = require('basic-auth');
 
-var auth = function (req, res, next) {
-  var user = basicAuth(req);
-  if (!user || !user.name || !user.pass) {
-    res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
-    res.sendStatus(401);
-    return;
-  }
-  if (user.name === 'paul' && user.pass === 'scribo825') {
-    next();
-  } else {
-    res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
-    res.sendStatus(401);
-    return;
-  }
-};
+
+
+
 
 
 // set the port of our application
@@ -42,7 +29,7 @@ var port = process.env.PORT || 8080;
 
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+//app.set('view engine', 'ejs');
 
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
@@ -52,8 +39,12 @@ app.get('/', auth, function(req, res) {
 
     // ejs render automatically looks in the views folder
     //res.render('index');
-    res.sendFile(__dirname + '/public/index.html');
+
+    // needed to put this in views otherwise public is just served without auth (fro lines above)
+    res.sendFile(__dirname + '/public/views/index.html');
 });
+
+
 
 
 
@@ -61,7 +52,7 @@ app.get('/', auth, function(req, res) {
 
 // router in its own thing
 
-var router     = require('./api/router');
+var router = require('./api/router');
 
 app.use('/api', router);
 
