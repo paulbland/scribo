79b4415e5bb7fe005149230326ff9e9c4be953da
Paul Bland <paul.bland@gmail.com>
2016-02-24 12:28:09 +1100

    cleaning up sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -66,7 +66,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
 
 
-  // Default task(s).
   grunt.registerTask('default', ['concurrent']);
 
 
--- public/js/app.js
+++ public/js/app.js
@@ -1,66 +1,66 @@
 var scribo = scribo || {};
 
-	$(function() {
+$(function() {
 
-		// Model
-		scribo.CardModel = Backbone.Model.extend({});
+	// Model
+	scribo.CardModel = Backbone.Model.extend({});
 
-		// Collection
-		scribo.CardCollection = Backbone.Collection.extend({
-		  	model: scribo.CardModel,
-		  	url: '/api/cards'
-		});
+	// Collection
+	scribo.CardCollection = Backbone.Collection.extend({
+	  	model: scribo.CardModel,
+	  	url: '/api/cards'
+	});
 
-		// Create new collectino
-	 	scribo.cards = new scribo.CardCollection();
+	// Create new collectino
+ 	scribo.cards = new scribo.CardCollection();
 
-	 	// get cards collection
-		scribo.cards.fetch();
-
-
-
-	  scribo.AppView = Backbone.View.extend({
-	     el: $('#cards'),
-
-	     initialize: function() {
-			this.listenTo(scribo.cards, 'reset', this.addAll);
-     		scribo.cards.fetch({reset: true});
-        },
-
-		addOne: function (card) {
-			var view = new scribo.CardView({ model: card });
-			this.$el.append(view.render().el);
-		},
-
-		// Add all items in the **Todos** collection at once.
-		addAll: function () {
-			scribo.cards.each(this.addOne, this);
-
-			$( "#cards" ).sortable();
-			$( "#cards" ).disableSelection();
-		}
-	}); 
+ 	// get cards collection
+	scribo.cards.fetch();
 
 
-	  scribo.CardView = Backbone.View.extend({
 
-	  	tagName: 'li',
+  scribo.AppView = Backbone.View.extend({
+     el: $('#cards'),
 
-	  	className: function() {
-	  		return 'card style-' + this.model.get('color');
-	  	},
+     initialize: function() {
+		this.listenTo(scribo.cards, 'reset', this.addAll);
+ 		scribo.cards.fetch({reset: true});
+    },
 
-	     template: _.template( $('#card-template').html() ),
+	addOne: function (card) {
+		var view = new scribo.CardView({ model: card });
+		this.$el.append(view.render().el);
+	},
 
-	     render: function () {
+	// Add all items in the **Todos** collection at once.
+	addAll: function () {
+		scribo.cards.each(this.addOne, this);
 
-	     		this.$el.html(this.template(this.model.toJSON()));
-	     		return this;
-	     	}
+		$( "#cards" ).sortable();
+		$( "#cards" ).disableSelection();
+	}
+}); 
 
-	  });
 
-	 new scribo.AppView();
+  scribo.CardView = Backbone.View.extend({
+
+  	tagName: 'li',
+
+  	className: function() {
+  		return 'card style-' + this.model.get('color');
+  	},
+
+     template: _.template( $('#card-template').html() ),
+
+     render: function () {
+
+     	this.$el.html(this.template(this.model.toJSON()));
+     	return this;
+     }
+
+  });
+
+ new scribo.AppView();
 
 
 		
