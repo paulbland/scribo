a764154237996a66ed658a95f2c5cf743ba9058c
Paul Bland <pbland@vsapartners.com>
2017-08-07 11:59:43 -0400

    changed config var name

--- api/router.js
+++ api/router.js
@@ -7,7 +7,7 @@ var router      = express.Router();
 // Set up JST on all router
 router.use(expressJwt({
     secret: new Buffer(process.env.JWT_SECRET, 'base64'),
-    audience: process.env.AUTH0_CLIENT_ID
+    audience: process.env.AUTH_CLIENT_ID
 }));
 
 
