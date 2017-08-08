915a91ccb331c756b47267fda538ca51ad93ea82
Paul Bland <pbland@vsapartners.com>
2017-08-08 16:04:09 -0400

    maybe i just forgot bower?

--- Gruntfile.js
+++ Gruntfile.js
@@ -166,6 +166,7 @@ module.exports = function(grunt) {
   
   grunt.loadNpmTasks('grunt-nodemon');
   grunt.loadNpmTasks('grunt-sass');
+ // grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-concurrent');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-jshint');
