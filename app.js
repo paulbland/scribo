6f8ee06b303b1ab81a19b9c4a90b2f6f0d55a34b
Paul Bland <paul.bland@gmail.com>
2021-03-10 21:33:08 -0500

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(favicon(__dirname + '/client/dist/img/favicon.ico'));
 
 // DATASBASE
 //mongoose.connect(process.env.PROD_MONGODB); // connect to our database
-var promise = mongoose.connect(process.env.PROD_MONGODB, { useMongoClient: true });
+var promise = mongoose.connect(process.env.PROD_MONGODB, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 
 // BASIC AUTH
