82327fb6570687b305be5ed9719d582456f6b2f5
Paul Bland <paul.bland@gmail.com>
2021-03-31 05:26:44 -0400

    Move underscore to npm

--- Gruntfile.js
+++ Gruntfile.js
@@ -122,7 +122,7 @@ module.exports = function(grunt) {
         src: [
           'node_modules/jquery/dist/jquery.js', /* was 'bower_components/jquery/dist/jquery.js' */
           'bower_components/sortablejs/Sortable.js', 
-          'bower_components/underscore/underscore.js',
+          'node_modules/underscore/underscore.js',
           'bower_components/backbone/backbone.js',
           'bower_components/handlebars/handlebars.js',
           'client/dist/js/templates.js',
