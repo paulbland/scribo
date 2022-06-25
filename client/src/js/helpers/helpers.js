72c70df8cd3ccee276301c3ba4b41a4440d6d536
Paul Bland <paul.bland@gmail.com>
2022-06-25 08:53:47 -0400

    Formatting tweaks

--- client/src/js/helpers/helpers.js
+++ client/src/js/helpers/helpers.js
@@ -1,15 +1,12 @@
 
 var Handlebars = require('handlebars/runtime');
 
-
 Handlebars.registerHelper('if_eq', function(a, b, opts) {
     if(a == b) // Or === depending on your needs
         return opts.fn(this);
     else
         return opts.inverse(this);
 });
-
-
 
 Handlebars.registerHelper('check_prop', function(a, b, c, opts) {
    if (a[b] === c) {
@@ -18,7 +15,6 @@ Handlebars.registerHelper('check_prop', function(a, b, c, opts) {
        return opts.inverse(this);
    }
 });
-
 
 Handlebars.registerHelper('first_to_upper', function(a, options) {
     return a.charAt(0).toUpperCase() + a.slice(1);
