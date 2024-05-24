5be54aaa038089c1b0d80c1a2ce05f17e49d4120
Paul Bland <paul.bland@gmail.com>
2024-05-24 00:10:43 -0400

    small tweaks

--- client-react/webpack.config.js
+++ client-react/webpack.config.js
@@ -17,11 +17,7 @@ module.exports = {
         },
       }, 
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.scss$/,
+        test: /\.(scss|css)$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
