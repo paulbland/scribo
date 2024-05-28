0f2f39aa3219bcb11b2779348f85170d690025f4
Paul Bland <paul.bland@gmail.com>
2024-05-27 23:40:34 -0400

    react looking pretty good

--- client-react/src/components/Card/Card.js
+++ client-react/src/components/Card/Card.js
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Card.scss'
 
-const Card = (props) => {
+const Card = (props) => {  
+  const cardStyles = [1,2,3,4,5];
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  // color is state and might chnge - update
+  // text might be too i guess
   return (
-    <>
-        <div>Single Card</div>
-        <p>Data from API in card: {props?.card?.text ? JSON.stringify(props.card.text) : 'Loading...'}</p>
-    </>
+    <li className="card">
+      <div className={"wrapper style-" + (props.card.color) + (isFlipped ? ' flipped' : '')}> 
+        <div className="front">
+          <textarea placeholder={"Type here..."} value={props.card.text}></textarea>
+          <a className="flip-card" onClick={() => setIsFlipped(!isFlipped)}></a>
+        </div>
+        <div className="back">
+          Select color:
+          <form>
+            <ul className="card-style">
+              {cardStyles.map((style) => {
+                return (
+                  <li key={style} className={"card-style-" + style}>
+                    <label>
+                      <input type="radio" name="card_style" value={style} checked={props.card.color === style} />
+                      <span></span>
+                    </label>
+                  </li>
+                )
+              })}
+            </ul>
+          </form>
+          <a href="#" className="delete-card">Delete this card</a><br />
+          <a className="flip-card" onClick={() => setIsFlipped(!isFlipped)}></a>
+        </div>
+      </div>
+    </li>
   )
 }
 
--- client-react/src/components/Cards/Cards.js
+++ client-react/src/components/Cards/Cards.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Card from '../Card/Card';
 import './Cards.scss'
 
-const Cards = (props) => {   
-  return (
-    <>
-        <div>All Cards</div>
-        {props.cards && props.cards.map((card) => {
-            return <Card key={card._id} card={card} />
-        })}
-    </>
+const Cards = () => {   
+  const [cards, setCards] = useState(null);
+
+  useEffect(() => {
+    const getCards = async () => {
+      try {
+        const response = await fetch('http://localhost:5006/api/cards', {
+          headers: new Headers({
+            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYXV0aDB8NTZlMzdlZmRlNWJiMTgzZjE3YzY2NDRiIiwiY2xpZW50SUQiOiJlYVhZRFZrSEhTTFFaVnRKc1hBckFIT0dxMjBZeG8xbCIsImVtYWlsIjoicGF1bC5ibGFuZEBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwidXBkYXRlZF9hdCI6IjIwMjQtMDUtMjdUMTk6MzM6MDQuNzA3WiIsInBpY3R1cmUiOiJodHRwczovL3MuZ3JhdmF0YXIuY29tL2F2YXRhci8xYWUxOTIxYWEzOTljOWFjNTM4ZjU3NjcwZmM0MmE3NT9zPTQ4MCZyPXBnJmQ9aHR0cHMlM0ElMkYlMkZjZG4uYXV0aDAuY29tJTJGYXZhdGFycyUyRnBhLnBuZyIsIm5hbWUiOiJwYXVsLmJsYW5kQGdtYWlsLmNvbSIsIm5pY2tuYW1lIjoicGF1bC5ibGFuZCIsImlkZW50aXRpZXMiOlt7InVzZXJfaWQiOiI1NmUzN2VmZGU1YmIxODNmMTdjNjY0NGIiLCJwcm92aWRlciI6ImF1dGgwIiwiY29ubmVjdGlvbiI6IlVzZXJuYW1lLVBhc3N3b3JkLUF1dGhlbnRpY2F0aW9uIiwiaXNTb2NpYWwiOmZhbHNlfV0sImNyZWF0ZWRfYXQiOiIyMDE2LTAzLTEyVDAyOjI5OjE3LjUyM1oiLCJsYXN0X3Bhc3N3b3JkX3Jlc2V0IjoiMjAyNC0wNS0xM1QxMzo0NjoxOS41MzVaIiwidXNlcl9tZXRhZGF0YSI6e30sImFwcF9tZXRhZGF0YSI6e30sImlzcyI6Imh0dHBzOi8vc2NyaWJvLmF1dGgwLmNvbS8iLCJzdWIiOiJhdXRoMHw1NmUzN2VmZGU1YmIxODNmMTdjNjY0NGIiLCJhdWQiOiJlYVhZRFZrSEhTTFFaVnRKc1hBckFIT0dxMjBZeG8xbCIsImlhdCI6MTcxNjg0MDkzMSwiZXhwIjoxNzE2ODc2OTMxfQ.1TtjSLn_bvZDPGbryoJvFABwJBxHnZAtbfnXFoCQ-IM' 
+          })
+        })
+        const data = await response.json();
+        setCards(data);
+      } catch (err) {
+        // console.error(err);
+      }
+    };
+    getCards();
+  }, []);
+
+  return (      
+    <ul id="cards">
+      {cards && cards.map((card) => {
+        return <Card key={card._id} card={card} />
+      })}
+    </ul>
   )
 }
 
